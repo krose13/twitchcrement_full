947b6bd80187a2ea545adcd2b6ddb0ef6fa84941
krose13 <keith.jarid.rose@gmail.com>
2017-02-15 21:08:39 +0000

    cleaning up scala comments

--- twitchcrement/src/main/scala/com/lotusprince/twitchcrement/KafkaConnector.scala
+++ twitchcrement/src/main/scala/com/lotusprince/twitchcrement/KafkaConnector.scala
@@ -17,7 +17,7 @@ import java.util.Properties
 
 object KafkaConnector{
 
-/** Parse a spam message into a Tuple5 for union with chatmessage */
+       /** Parse a spam message into a Tuple5 for union with chatmessage */
 
        def InterpretSpamMessage(m1:String): Tuple5[String, Int, Int, String, Int] = {
        	   var mTup: Tuple5[String, Int, Int, String, Int] = ("", 0, 0, "", 0)
@@ -35,10 +35,10 @@ object KafkaConnector{
 	   return mTup
 	   }
 
-/** Reduce function to combine spam messages with chat messages.
-    If 2 spam messages arrive in sequence, then reset the count completely becuase the topic isn't active anymore.
-    Otherwise add the new user to a list of uniques or increment the count if not a new unique
-    Then update the number of active and total spam messages. */
+	   /** Reduce function to combine spam messages with chat messages.
+	       If 2 spam messages arrive in sequence, then reset the count completely becuase the topic isn't active anymore.
+               Otherwise add the new user to a list of uniques or increment the count if not a new unique
+	       Then update the number of active and total spam messages. */
 
        def CombineTuples(t1:Tuple5[String, Int, Int, String, Int], t2:Tuple5[String, Int, Int, String, Int]): Tuple5[String, Int, Int, String, Int] = {
        	   var rTup:Tuple5[String, Int, Int, String, Int] = ("",0,0,"", 0)
@@ -107,56 +107,47 @@ object KafkaConnector{
 	   val toRemove = "�".toSet
 
 
-/** chat messages */
 
-	   val streamMessages =stream
+
+	   val streamMessages =stream /** chat messages */
 	       .map { w => w.filterNot(toRemove) }
 	       .map { w => ((w.split(" ")(w.indexOf("channel")+1).toString() + " " + w.split(" ").drop(6).mkString(" ")), 1, 0, (w.split(" ")(4).toString() + " 1"), 2)}
 
 
-
-/** Acquire list of most recent spam terms */
-
 	   val SpamStream = env.addSource(new FlinkKafkaConsumer09[String]("spammessage", new SimpleStringSchema(), p))
+	   /** Acquire list of most recent spam terms */
 
-/** Process Spam terms */
-
-	   val processedSpamStream = SpamStream
+	   val processedSpamStream = SpamStream  /** Process Spam terms */
 	       .map{ w => InterpretSpamMessage(w) }
 
 	       
-/** Union and reduce stream with chat and spam messages */
+    
+
 
 	   val streamWithSpam = streamMessages.union(processedSpamStream)
 	       .keyBy(0)
 	       .reduce((left, right) => CombineTuples(left, right))
+	       /** Union and reduce stream with chat and spam messages */
 
 	   val printSpamStream = processedSpamStream
 	       .map {w => w.productIterator.mkString(" ") }
 
 
-/** Filtered messages that occur repeatedly */
-
-           val windowSpamCounts = streamWithSpam
+           val windowSpamCounts = streamWithSpam  /** Filter messages that occur repeatedly */
                .keyBy(0)
                .timeWindow(Time.seconds(10))
 	       .maxBy(1)
                .filter{ w => w._2 > 1 }  
 	       .map { w => ( w._1, 0, w._3+w._2, w._4, w._5) }
 
+
 	   val finalSpamCounts = windowSpamCounts
                .map { w => w.productIterator.mkString(" ") }
-
-	   val cassandraJavaCounts = windowSpamCounts 
-	       .map{ w => (w._1.split(" ")(0), w._1.split(" ").drop(1).mkString(" "), System.currentTimeMillis, (w._5/2).toString()) }
  	   
-/** Print to log */
+	   finalSpamCounts.print().setParallelism(1)  /** Print to log */
 
-	   finalSpamCounts.print().setParallelism(1)  
-
-/** Push to Kafka again */
-	   finalSpamCounts.addSink(new FlinkKafkaProducer09[String](sys.env("KAFKAPORT").toString, "spammessage", new SimpleStringSchema()))
-
+	   finalSpamCounts  /** Push to Kafka again */
+		.addSink(new FlinkKafkaProducer09[String](sys.env("KAFKAPORT").toString, "spammessage", new SimpleStringSchema()))
 
 	   env.execute("KafkaConnector")
        }
